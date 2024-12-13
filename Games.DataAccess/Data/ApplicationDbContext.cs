761d9dd0032ab5e738db29fa0b54621365cd30ad
Hamza <hamzabasri@OUTLOOK.FR>
2024-12-13 11:48:06 -0500

    Ajout et configuration de IdentityUser

--- Games.DataAccess/Data/ApplicationDbContext.cs
+++ Games.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿
 using Games.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Games.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -14,6 +16,7 @@ namespace Games.DataAccess.Data
         public DbSet<Game> Games { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Adventure", DisplayOrder = 2 },
